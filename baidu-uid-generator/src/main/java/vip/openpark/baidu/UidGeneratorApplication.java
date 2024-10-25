cb6a48c45f670c0f5ce0fc2ec0006c9f1afebf0f
anthony <anthony1949@zto.com>
2024-10-25 13:31:17 +0800

    feature：baidu id generator 添加必要注释

--- baidu-uid-generator/src/main/java/vip/openpark/baidu/UidGeneratorApplication.java
+++ baidu-uid-generator/src/main/java/vip/openpark/baidu/UidGeneratorApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * 前置条件
+ * 1. 基于 jdk8/maven 进行 install 到本地或者 deploy 到 maven 私服。mvn clean install -DskipTests
+ * 2. 创建自己的项目并依赖 UidGenerator
+ * 3. 创建数据库及 work_node 表：<a href="https://github.com/baidu/uid-generator/blob/master/src/main/scripts/WORKER_NODE.sql">...</a>
+ *
  * @author anthony
  * @version 2024-10-24
  * @since 2024-10-24 10:53
